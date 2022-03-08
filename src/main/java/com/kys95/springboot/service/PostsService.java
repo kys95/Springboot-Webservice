747778a9142612a278315afa99d0ebe31146758a
Yunseok Kang <mok02198@gmail.com>
2022-03-08 22:21:47 +0900

    Feat: 게시글 등록&전제 조회 기능 생성

--- src/main/java/com/kys95/springboot/service/PostsService.java
+++ src/main/java/com/kys95/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.kys95.springboot.service;
 
 import com.kys95.springboot.domain.posts.Posts;
 import com.kys95.springboot.domain.posts.PostsRepository;
+import com.kys95.springboot.web.dto.PostsListResponseDto;
 import com.kys95.springboot.web.dto.PostsResponseDto;
 import com.kys95.springboot.web.dto.PostsSaveRequestDto;
 import com.kys95.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,5 +40,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
