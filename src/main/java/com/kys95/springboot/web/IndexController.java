10cdc1c8f1d59ae306fbd2596dd117bf7761d431
Yunseok Kang <mok02198@gmail.com>
2022-03-12 23:54:42 +0900

    Feat: 로그인 테스트

--- src/main/java/com/kys95/springboot/web/IndexController.java
+++ src/main/java/com/kys95/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.kys95.springboot.web;
 
+import com.kys95.springboot.config.auth.dto.SessionUser;
 import com.kys95.springboot.service.PostsService;
 import com.kys95.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
