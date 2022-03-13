08ac6f8b68854edef9d8faa6bd54d96126895064
Yunseok Kang <mok02198@gmail.com>
2022-03-13 16:23:17 +0900

    Refactor: 세션값 가져오는 코드 반복되는 부분 개선

--- src/main/java/com/kys95/springboot/web/IndexController.java
+++ src/main/java/com/kys95/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.kys95.springboot.web;
 
+import com.kys95.springboot.config.auth.LoginUser;
 import com.kys95.springboot.config.auth.dto.SessionUser;
 import com.kys95.springboot.service.PostsService;
 import com.kys95.springboot.web.dto.PostsResponseDto;
@@ -15,13 +16,11 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class IndexController {
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
+        
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
